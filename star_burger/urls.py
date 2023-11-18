608e2b10af184da1651877dc8daf95531271326a
den1slx <keycoingold@mail.ru>
2023-11-18 19:04:05 +1000

    Update urls.py

--- star_burger/urls.py
+++ star_burger/urls.py
@@ -19,7 +19,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.shortcuts import render
 
-from . import settings
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
